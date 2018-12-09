25c90eb01021d54dce912bfd8b4e1ba4943567d7
HWittlich <keks1289@gmx.de>
2018-12-09 01:12:12 +0100

    add address creation feature

---
+++ TMS/src/main/java/de/htw/ai/hagen/TMS/AddressManager.java
@@ -0,0 +1,39 @@
+package de.htw.ai.hagen.TMS;
+
+import java.util.Random;
+
+public class AddressManager {
+	
+	public static int coordinatorAddress = 0x0000;
+		
+	private static int temporaryAddressesUpperBound = 0x00FF;
+	private static int temporaryAddressesLowerBound = 0x0011;
+	
+	private static int permanentAddressesUpperBound = 0xFFFE;
+	private static int permanentAddressesLowerBound = 0x0011;
+	
+	public static Integer lastGivenAddress = null; 
+		
+	/**
+	 * this method is used by the Node on startup. It is necessary to request a permanent address.
+	 * @return
+	 */
+	public static String createTemporaryNodeAddress() {
+		Random rand = new Random();
+		int myRandomNumber = rand.nextInt(temporaryAddressesUpperBound-temporaryAddressesLowerBound) + temporaryAddressesLowerBound; // Generates a random number between 0011 and 00FF (00EE+0011)
+		System.out.printf("%x\n",myRandomNumber); // Prints it in hex, such as "0x14"
+		// or....
+		return String.format("%1$04X", myRandomNumber); // Random hex number in result
+	}
+	
+	
+	// TODO this could be better handled by an address Collection itself
+	public static String generateNextFreePermanentAddress() {
+		if (lastGivenAddress == null) {
+			return String.format("%1$04X",permanentAddressesLowerBound);
+		}
+		return String.format("%1$04X",lastGivenAddress+1);
+	}
+
+
+}
