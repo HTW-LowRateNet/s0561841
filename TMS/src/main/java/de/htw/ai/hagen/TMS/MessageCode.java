c1e4994059622b410f57d36d1a41248c97a6c07b
HWittlich <keks1289@gmx.de>
2018-12-09 01:13:33 +0100

    add NRST

--- TMS/src/main/java/de/htw/ai/hagen/TMS/MessageCode.java
+++ TMS/src/main/java/de/htw/ai/hagen/TMS/MessageCode.java
@@ -5,15 +5,18 @@ public enum MessageCode {
 	/** Code for neighbor discovery. */
 	DISC("DISC"),
 	/** Code for discovering the current Coordinator of the network */
-	KDIS("KDIS"),
+	CDIS("CDIS"),
 	/** Code for Setting the Node's address */
 	ADDR("ADDR"),
 	/** Code for a simple message */
 	MSSG("MSSG"),
 	/** Code for self-polling to detect address collision */
 	POLL("POLL"),
-	/** Code for self-polling to detect address collision */
-	ALIV("ALIV");
+	/** Code for letting network know that this node is the coordinator */
+	ALIV("ALIV"), 
+	/**	Code for letting the network know that it needs to be reset */
+	NRST("NRST");
+
 
 	protected String code;
 
