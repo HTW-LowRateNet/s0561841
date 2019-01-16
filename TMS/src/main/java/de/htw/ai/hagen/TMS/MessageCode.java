138a80c2876aa07b229bed8262eb555c0ab66b7d
HWittlich <keks1289@gmx.de>
2019-01-16 18:20:45 +0100

    removed unused codes

--- TMS/src/main/java/de/htw/ai/hagen/TMS/MessageCode.java
+++ TMS/src/main/java/de/htw/ai/hagen/TMS/MessageCode.java
@@ -2,16 +2,13 @@ package de.htw.ai.hagen.TMS;
 
 public enum MessageCode {
 
-	/** Code for neighbor discovery. */
-	DISC("DISC"),
+
 	/** Code for discovering the current Coordinator of the network */
 	CDIS("CDIS"),
 	/** Code for Setting the Node's address */
 	ADDR("ADDR"),
 	/** Code for a simple message */
 	MSSG("MSSG"),
-	/** Code for self-polling to detect address collision */
-	POLL("POLL"),
 	/** Code for letting network know that this node is the coordinator */
 	ALIV("ALIV"), 
 	/**	Code for letting the network know that it needs to be reset */
